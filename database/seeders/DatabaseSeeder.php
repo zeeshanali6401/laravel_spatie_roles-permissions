79736d07bbb8ceed3c7a9cfc77d7821d51671f9b
zeeshanali <zeeshanali6401@gmail.com>
2023-10-24 11:55:24 +0500

    commited

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +15,21 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-    }
+{
+    Permission::create(['name' => 'add']);
+    Permission::create(['name' => 'update']);
+    Permission::create(['name' => 'show']);
+    Permission::create(['name' => 'delete']);
+
+    $role = Role::create(['name' => 'admin']);
+    $role->givePermissionTo(['add', 'show', 'update', 'delete']);
+
+    \App\Models\User::factory()->create([
+        'name' => 'Super Admin',
+        'email' => 'admin@admin.com',
+        'password' => '$2y$10$qCsQvAhKdYgTBIFOXDuLfeh42w11g/EgASMOuOB74Fr8gaVhfJC0i',  // password
+        'role'  =>  'admin',
+    ])->assignRole('admin');
+}
+
 }
