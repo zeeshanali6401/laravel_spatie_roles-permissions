6fdf8871dfcd4aca6397393412d82f8318f18652
Zeeshan <zeeshanali6401@gmail.com>
2023-10-30 19:15:47 +0500

    commited

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,8 +26,8 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand" href="{{ url('/') }}/{{ auth()->user()->name }}/home">
+                    Laravel
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
